203989f943fb957fe69a20cb4b89a4306eb88ac1
Dev Prit <dev.prit@lcinocm.com>
2023-04-09 16:20:19 +0700

    Fixed apply object email to App

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   register({ app, type }) {
     let servers = {};
 
-    app.email = function (g = null) {
+    App.email = function (g = null) {
       g = g || 'default';
       if (!servers[g]) servers[g] = new Email(App.config.email[g]);
       return servers[g];
