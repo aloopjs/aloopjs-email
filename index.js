257a492b20c5312ed8f8ce10b28d4adf0338c083
Dev Prit <dev.prit@lcinocm.com>
2023-04-10 15:56:33 +0700

    rename App to Aloop

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ module.exports = {
   register({ app, type }) {
     let servers = {};
 
-    App.email = function (g = null) {
+    Aloop.email = function (g = null) {
       g = g || 'default';
-      if (!servers[g]) servers[g] = new Email(App.config.email[g]);
+      if (!servers[g]) servers[g] = new Email(Aloop.config.email[g]);
       return servers[g];
     };
   },
